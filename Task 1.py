cfef946e966636c7153e94fec08d707c2868a727
Azeeemaaa <azeeemaaausa@gmail.com>
2025-05-05 13:07:30 -0700

    Задание 1
    Выполнил первое задание и пробую  запушить

---
+++ Task 1.py
@@ -0,0 +1,21 @@
+# Задание 1: Типы переменных
+"""Создай переменные следующих типов:
+целое число
+дробное число
+строка
+булево значение
+список
+и выведи их типы с помощью type()."""
+
+# Ответ:
+age = 31
+height = 1.75
+last_name = 'Kulov'
+human = True
+food_like = ['nuttella', 'plov', 'pizza']
+
+print(type(age))
+print(type(height))
+print(type(last_name))
+print(type(human))
+print(type(food_like))
