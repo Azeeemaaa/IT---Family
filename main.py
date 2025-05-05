6cc33dd4a7e4181fb2941fa50ff3d7007f0626ae
Azeeemaaa <azeeemaaausa@gmail.com>
2025-05-05 12:43:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 age = int(input("Enter your age: "))
 if age < 18:
-    print("You are a minor.")
+    print("You are a minor.")       # This line is correct
 
-elif age >= 18 and age < 65:
+elif age >= 18 and age < 65:        # This line is disabled
     print("You are an adult.")
 
-else age >= 65:
+else age >= 65:   # This line is incorrect
     print("You are a senior citizen.")
